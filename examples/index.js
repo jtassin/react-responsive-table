175fc78fa148c202f60cb968e605c18f5dd3bf77
Julien Tassin <jtassin@artprice.com>
2016-08-04 18:08:49 +0200

    init

---
+++ examples/index.js
@@ -0,0 +1,9 @@
+import normalizeCss from './stylesheets/normalize.css';
+import githubLightCss from './stylesheets/github-light.css';
+import stylesheetCss from './stylesheets/stylesheet.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Table, Row, Cell, UnwrappableCells} from '../src/index';
+
+'example-0'
---
+++ src/Table/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Table extends React.Component {
+    
+    static styles = {
+        
+    }
+    
+    render() {
+        <div></div>
+    }
+}
+
+export default Table;
