fb6aaf398e51637b4358e58432add10e67622d8f
Julien <julien@tassin.work>
2016-08-17 21:54:15 +0200

    add examples

--- examples/index.js
+++ examples/index.js
@@ -6,6 +6,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Table, Row, Cell, UnwrappableCells} from '../src/index';
 
-ReactDOM.render((<Table mdl><Row><Cell minWidthPx={100}>Col-1-1</Cell><Cell minWidthPx={100}>Long text for Col-1-2</Cell></Row><Row><Cell minWidthPx={100}>Col-2-1</Cell><Cell minWidthPx={100}>Col-2-2</Cell></Row><Row><Cell minWidthPx={100}>Long text for Col-3-1</Cell><Cell minWidthPx={100}>Col-3-2</Cell></Row></Table>), document.getElementById('example-0'));
+ReactDOM.render((
+    <Table mdl>
+        <Row>
+            <Cell minWidthPx={100}>Col-1-1</Cell>
+            <Cell minWidthPx={100}>Long text for Col-1-2</Cell>
+        </Row>
+        <Row>
+            <Cell minWidthPx={100}>Col-2-1</Cell>
+            <Cell minWidthPx={100}>Col-2-2</Cell>
+        </Row>
+        <Row>
+            <Cell minWidthPx={100}>Long text for Col-3-1</Cell>
+            <Cell minWidthPx={100}>Col-3-2</Cell>
+        </Row>
+    </Table>), document.getElementById('example-0'));
 
 
+ReactDOM.render((
+    <Table mdl>
+        <Row>
+            <Cell minWidthPx={200}>Col-1-1</Cell>
+            <Cell minWidthPx={100}>Col-1-2</Cell>
+            <Cell minWidthPx={100}>Col-1-3</Cell>
+        </Row>
+        <Row>
+            <Cell minWidthPx={200}>Col-2-1</Cell>
+            <Cell minWidthPx={100}>Col-2-2</Cell>
+            <Cell minWidthPx={100}>Col-2-3</Cell>
+        </Row>
+    </Table>), document.getElementById('example-1'));
