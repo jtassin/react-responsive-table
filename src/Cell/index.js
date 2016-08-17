96abf7b4117b33ed5da73d6451df583c431b90b6
Julien <julien@tassin.work>
2016-08-17 22:34:49 +0200

    eslint

--- src/Cell/index.js
+++ src/Cell/index.js
@@ -3,37 +3,38 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 class Cell extends Component {
 
-    static propTypes = {
-        minWidthPx: PropTypes.number,
-        style: PropTypes.object,
-    };
-    
-    static styles = {
-        cell: {
-            flexGrow: 1,
-            overflow: 'hidden',
-            verticalAlign: 'top',
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis',
-            height: '48px',
-            padding: '12px 18px 12px 24px',
-            boxSizing: 'border-box',
-        },
+  static propTypes = {
+    minWidthPx: PropTypes.number,
+    style: PropTypes.object,
+    children: PropTypes.object,
+  };
 
-    }
+  static styles = {
+    cell: {
+      flexGrow: 1,
+      overflow: 'hidden',
+      verticalAlign: 'top',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      height: '48px',
+      padding: '12px 18px 12px 24px',
+      boxSizing: 'border-box',
+    },
 
-    constructor(props) {
-        super(props);
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  render() {
+    const cellStyle = Object.assign({}, Cell.styles.cell, this.props.style);
+    if (this.props.minWidthPx) {
+      cellStyle.width = `${this.props.minWidthPx}px`;
     }
-    
-    render() {
-        const cellStyle = Object.assign({}, Cell.styles.cell, this.props.style);
-        if(this.props.minWidthPx) {
-            cellStyle.width = `${this.props.minWidthPx}px`
-        }
-        return <div style={cellStyle}>{this.props.children}</div>;
-    }
+    return <div style={cellStyle}>{this.props.children}</div>;
+  }
 }
 
 export default Cell;
--- src/Table/index.js
+++ src/Table/index.js
@@ -2,39 +2,39 @@ import React, { PropTypes, Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 class Table extends Component {
-    
-    static styles = {
-        table: {
-        },
-        mdlTable: {
-            boxShadow: '0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12)',
-            border: '1px solid rgba(0,0,0,.12)',
-            borderCollapse: 'collapse',
-            whiteSpace: 'nowrap',
-            fontSize: '13px',
-            backgroundColor: '#fff',
-            fontSize: '13px',
-            color: '#757575',
-        }
-    }
 
-    static propTypes = {
-        mdl: PropTypes.bool,
-        style: PropTypes.object,
-    };
+  static styles = {
+    table: {},
+    mdlTable: {
+      /* eslint-disable max-len */
+      boxShadow: '0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12)',
+      border: '1px solid rgba(0,0,0,.12)',
+      borderCollapse: 'collapse',
+      whiteSpace: 'nowrap',
+      backgroundColor: '#fff',
+      fontSize: '13px',
+      color: '#757575',
+    },
+  }
 
-    constructor(props) {
-        super(props);
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  static propTypes = {
+    mdl: PropTypes.bool,
+    style: PropTypes.object,
+    children: PropTypes.elem,
+  };
+
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  render() {
+    const tableStyle = Object.assign({}, Table.styles.table, this.props.style);
+    if (this.props.mdl) {
+      Object.assign(tableStyle, Table.styles.mdlTable);
     }
-    
-    render() {
-        const tableStyle = Object.assign({}, Table.styles.table, this.props.style);
-        if(this.props.mdl) {
-            Object.assign(tableStyle, Table.styles.mdlTable);
-        }
-        return <div style={tableStyle}>{this.props.children}</div>;
-    }
+    return <div style={tableStyle}>{this.props.children}</div>;
+  }
 }
 
 export default Table;
