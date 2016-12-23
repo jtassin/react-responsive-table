7e53a43ee828fcabf517a3602a244b1cabc97820
Julien Tassin <jtassin@artprice.com>
2016-12-23 15:48:11 +0100

    add Form example

---
+++ examples/src/FormExample.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Playground from 'component-playground';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { Table, Row, Cell } from '../../src/index';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+export default function render() {
+  return (
+    <div>
+      <div style={{ backgroundColor: 'white' }}>
+        <Playground
+          codeText={`<MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+  <Table material>
+  <form key="row1" onSubmit={(event) => { event.preventDefault(); alert('submitted row 1'); }}>
+      <Row>
+        <Cell minWidthPx={100} key="cell1"><TextField fullWidth hintText="Required field" required/></Cell>
+        <Cell minWidthPx={100} key="cell2"><TextField fullWidth hintText="Required field"/></Cell>
+        <Cell minWidthPx={200} key="cell3"><FlatButton type="submit" primary label="Submit Row 1"/></Cell>
+      </Row>
+    </form>
+    <form key="row2" onSubmit={(event) => { event.preventDefault(); alert('submitted row 2'); }}>
+      <Row>
+        <Cell minWidthPx={100} key="cell1"><TextField fullWidth hintText="Required field" required/></Cell>
+        <Cell minWidthPx={100} key="cell2"><TextField fullWidth hintText="Required field"/></Cell>
+        <Cell minWidthPx={200} key="cell3"><FlatButton type="submit" primary label="Submit Row 2"/></Cell>
+      </Row>
+    </form>
+  </Table>
+</MuiThemeProvider>`} scope={{ React, Table, Row, Cell, MuiThemeProvider, lightBaseTheme, getMuiTheme, TextField, FlatButton }}
+        />
+      </div>
+    </div>
+  );
+}
+
--- examples/src/example.jsx
+++ examples/src/example.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import BasicExample from './BasicExample';
+import FormExample from './FormExample';
 import MaterialExample from './MaterialExample';
 
 const RequiredMaterialExample = require('./MaterialExample').default;
 const RequiredBasicExample = require('./BasicExample').default;
+const RequiredFormExample = require('./FormExample').default;
 
 require('./github-light.css');
 require('./normalize.css');
@@ -21,6 +23,15 @@ ReactDOM.render(
 );
 
 /* eslint-disable no-undef */
+const requiredFormExampleEl = document.getElementById('form-example');
+ReactDOM.render(
+  <AppContainer>
+    <FormExample />
+  </AppContainer>,
+  requiredFormExampleEl,
+);
+
+/* eslint-disable no-undef */
 const basicExampleEl = document.getElementById('basic-example');
 ReactDOM.render(
   <AppContainer>
@@ -33,6 +44,7 @@ if (module.hot) {
   const examples = [
     { name: MaterialExample, node: <RequiredMaterialExample />, elem: materialExampleEl },
     { name: BasicExample, node: <RequiredBasicExample />, elem: basicExampleEl },
+    { name: FormExample, node: <RequiredFormExample />, elem: requiredFormExampleEl },
   ];
   examples.forEach((example) => {
     module.hot.accept(`./${example.name}`, () => {
