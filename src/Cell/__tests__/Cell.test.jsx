16272391697bb8f4e90facd4c8a5551e1008115f
Julien <julien@tassin.work>
2017-07-03 21:30:47 +0200

    moving to jest snapshot mode

--- src/Cell/__tests__/Cell.test.jsx
+++ src/Cell/__tests__/Cell.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { expect } from 'chai';
 import sinon from 'sinon';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -10,8 +9,6 @@ import { Cell } from '../../index.js';
 global.navigator = { userAgent: 'all' };
 
 describe('Cell', () => {
-
-  const DEFAULT_CELL_RESULT = '<div style="display:flex;flex-grow:1;overflow:hidden;vertical-align:middle;text-overflow:ellipsis;"><div style="margin:auto;"></div></div>';
 
   // following lines serve to detect bad proptype or any react warning
 
@@ -26,28 +23,28 @@ describe('Cell', () => {
         let wrapper = shallow(
             <Cell/>
         );
-        expect(wrapper.html()).to.equal(DEFAULT_CELL_RESULT);
+        expect(wrapper).toMatchSnapshot();
     });
 
   it('use minWidthPx property', () => {
     let wrapper = shallow(
       <Cell minWidthPx={100}/>
     );
-    expect(wrapper.html()).to.equal('<div style="display:flex;flex-grow:1;overflow:hidden;vertical-align:middle;text-overflow:ellipsis;width:100px;"><div style="margin:auto;"></div></div>');
+    expect(wrapper).toMatchSnapshot();
   });
 
     it('merge styles passed as argument to component style', () => {
         let wrapper = shallow(
             <Cell style={{ color: 'red' }}/>
         );
-        expect(wrapper.html()).to.equal('<div style="display:flex;flex-grow:1;overflow:hidden;vertical-align:middle;text-overflow:ellipsis;"><div style="margin:auto;color:red;"></div></div>');
+        expect(wrapper).toMatchSnapshot();
     });
 
     it('render inside the cell the children', () => {
         let wrapper = shallow(
             <Cell><span>Content</span></Cell>
         );
-      expect(wrapper.html()).to.equal('<div style="display:flex;flex-grow:1;overflow:hidden;vertical-align:middle;text-overflow:ellipsis;"><div style="margin:auto;"><span>Content</span></div></div>');
+      expect(wrapper).toMatchSnapshot();
     });
 
     describe('material', () => {
@@ -55,28 +52,28 @@ describe('Cell', () => {
         let wrapper = shallow(
           <Cell header={true}/>
         );
-        expect(wrapper.html()).to.equal(DEFAULT_CELL_RESULT);
+        expect(wrapper).toMatchSnapshot();
       });
 
       it('does not fail if context.muiTheme is missing', () => {
         let wrapper = shallow(
           <Cell material={true}/>
         );
-        expect(wrapper.html()).to.equal(DEFAULT_CELL_RESULT);
+        expect(wrapper).toMatchSnapshot();
       });
 
       it('use the material ui theme', () => {
         let wrapper = shallow(
           Cell({material: true}, { muiTheme: getMuiTheme(lightBaseTheme) })
         );
-        expect(wrapper.html()).to.equal('<div style="display:flex;flex-grow:1;overflow:hidden;vertical-align:middle;text-overflow:ellipsis;padding-left:24px;padding-right:24px;min-height:48px;text-align:left;font-size:13px;"><div style="margin:auto;"></div></div>');
+        expect(wrapper).toMatchSnapshot();
       });
 
       it('add correct style if header is true', () => {
         let wrapper = shallow(
           Cell({material: true, header: true}, { muiTheme: getMuiTheme(lightBaseTheme) })
         );
-        expect(wrapper.html()).to.equal('<div style="display:flex;flex-grow:1;overflow:hidden;vertical-align:middle;text-overflow:ellipsis;padding-left:24px;padding-right:24px;min-height:48px;text-align:left;font-weight:normal;font-size:12px;color:#9e9e9e;position:relative;"><div style="margin:auto;"></div></div>');
+        expect(wrapper).toMatchSnapshot();
       });
     });
 });
